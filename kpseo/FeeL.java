224596a9ed39dae8f788ccbfbc531c9775ee6ecb
kpseo <skp10216@nate.com>
2010-11-02 13:28:14 +0900

    2010.11.02 study

---
+++ kpseo/FeeL.java
@@ -0,0 +1,15 @@
+
+public class FeeL {
+	public static void main(String[] args)
+	{
+		char ch = 'a';
+		int nt = (int)ch;
+		System.out.println(ch);
+		System.out.println(nt);
+		
+		int num = 97;
+		char code = (char)num;
+		System.out.println(code);
+		System.out.println(nt-code);
+	}	
+}
